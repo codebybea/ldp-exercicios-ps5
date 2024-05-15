f60f865fedfb45f06de43164417dd2b2af5daea7
Beatriz Alves <117928932+codebybea@users.noreply.github.com>
2024-05-15 17:43:26 -0300

    att exc

--- p3.js
+++ p3.js
@@ -1,3 +1,14 @@
 // ATIVIDADE PS5 BEATRIZ BORGES - 23214290001
 
 // P3
+
+function sumAP(a, d, n) {
+    
+    const totalTerms = n;
+    const firstTerm = a;
+    const lastTerm = a + (n - 1) * d;
+    const sum = (totalTerms * (firstTerm + lastTerm)) / 2;
+  
+    return sum;
+  }
+  
--- p4.js
+++ p4.js
@@ -1,3 +1,19 @@
 // ATIVIDADE PS5 BEATRIZ BORGES - 23214290001
 
 // P4
+
+function sumGP(a, r, n) {
+
+    if (r === 1) {
+      return a * n;
+    } else {
+      const totalTerms = n;
+      const firstTerm = a;
+      const lastTerm = a * Math.pow(r, n - 1);
+      const sum = (firstTerm * (1 - Math.pow(r, n))) / (1 - r);
+      return sum;
+    }
+  }
+  const somaPG = sumGP(2, 3, 4);
+  console.log("Soma da PG:", somaPG); 
+  
